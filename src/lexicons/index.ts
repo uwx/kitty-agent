830502ada7d2ce63d42afbf60e5eb6fd1b0d1c46
uwx <maxine@cybergal.com>
2026-03-10 13:37:29 +0100

    fix index.ts

--- src/lexicons/index.ts
+++ src/lexicons/index.ts
@@ -3,5 +3,3 @@ export * as BlueMicrocosmLinksGetBacklinksCount from "./types/blue/microcosm/lin
 export * as BlueMicrocosmLinksGetManyToManyCounts from "./types/blue/microcosm/links/getManyToManyCounts.js";
 export * as ComBadExampleIdentityResolveMiniDoc from "./types/com/bad-example/identity/resolveMiniDoc.js";
 export * as ComBadExampleRepoGetUriRecord from "./types/com/bad-example/repo/getUriRecord.js";
-export * as IoGitlabKinklistKinklistList from "./types/io/gitlab/kinklist/kinklist/list.js";
-export * as IoGitlabKinklistKinklistProfile from "./types/io/gitlab/kinklist/kinklist/profile.js";
